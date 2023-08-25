1847173a500c2f094baaf036534d01b919da0402
Rahul Kundu <rahul.kundu>
2023-08-25 12:52:12 +0530

    Initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonInput, IonPage, IonText } from '@ionic/react';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
+  const [ value, setValue ] = useState<string | null | undefined>();
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+    <IonPage className="ion-padding-top">
+      <IonContent fullscreen className="ion-padding">
+        <IonInput
+          label='Input'
+          value={ value }
+          type="number"
+          onIonInput={ event => setValue(event.detail.value) }
+        />
+        <IonText>{ `Output value: ${value}` }</IonText>
       </IonContent>
     </IonPage>
   );
